127ed27ecc2e09f5d76dbf8f378628f9b542a0f8
donaldvallejo <donny.vallejo@gmail.com>
2017-10-04 18:21:06 -0700

    rebuilt in materialze

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+  //  the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal();
+ });
+
 var config = {
     apiKey: "AIzaSyB-gK7ArVYsICoeXOgvMBd0YYiZLx8HAZM",
     authDomain: "albertyufirebaseproject.firebaseapp.com",
@@ -20,6 +25,19 @@ var endLat;
 var endLng;
 var startLng;
 var startLat;
+
+$('.modal').modal({
+     dismissible: true, // Modal can be dismissed by clicking outside of the modal
+     opacity: .5, // Opacity of modal background
+     inDuration: 300, // Transition in duration
+     outDuration: 200, // Transition out duration
+     startingTop: '0%', // Starting top style attribute
+     endingTop: '0%', // Ending top style attribute
+     ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+       console.log(modal, trigger);
+     },
+   }
+ );
 
 $("#close").click(function() {
     $('.transform').toggleClass('transform-active');
